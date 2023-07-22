f467a2ad5e4a5189e821baa7ce55e87c8e6f2986
Tom Palmer <remlapmot@hotmail.com>
2023-07-22 20:47:25 +0100

    Create install-r.sh

---
+++ .devcontainer/scripts/install-r.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+apt-get update
+apt-get -y install --no-install-recommends software-properties-common dirmngr
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+apt-get update && apt-get -y install --no-install-recommends r-base
