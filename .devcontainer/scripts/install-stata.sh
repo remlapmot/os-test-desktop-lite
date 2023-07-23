312306bd262a660a0cc523c7b235a676d2578891
Tom Palmer <remlapmot@hotmail.com>
2023-07-23 16:47:11 +0100

    Fiddles for stata.lic

--- .devcontainer/scripts/install-stata.sh
+++ .devcontainer/scripts/install-stata.sh
@@ -5,4 +5,6 @@ cd /tmp/statafiles
 tar -zxf Stata17Linux64.tar.gz
 # mkdir /usr/local/stata17
 cd /usr/local/stata17
-/tmp/statafiles/install
+# run the next command interactively
+# /tmp/statafiles/install
+mv /tmp/statafiles/stata.lic .
