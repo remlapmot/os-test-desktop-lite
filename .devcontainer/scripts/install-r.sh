587b3ff3c74aa0fe655f19846bef46d332d5d4b9
Tom Palmer <remlapmot@hotmail.com>
2023-07-24 17:27:59 +0100

    Delete install-r.sh

--- .devcontainer/scripts/install-r.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-apt-get update
-apt-get -y install --no-install-recommends software-properties-common dirmngr
-wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
-add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
-apt-get update && apt-get -y install --no-install-recommends r-base
